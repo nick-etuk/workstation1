42204b3c72f4ee1ff4774a0b5e98a613be5d70f2
Nick Etuk <niet2@hscic.gov.uk>
2025-12-21 17:26:44 +0000

    Added Powershell aliases

--- src/core/lib/environment/add_aliases.ps1
+++ src/core/lib/environment/add_aliases.ps1
@@ -1,8 +1,15 @@
 function add_aliases {
+    write-output "=>add_aliases"
     if (get-alias | findstr 'grep') { return }
+    write-output "bp1 add_aliases running"
 
     New-Alias grep findstr
-    New-Alias gch git checkout
-    New-Alias gs git status
+    New-Alias gch 'git checkout'
+    New-Alias gs 'git status'
     New-Alias gls 'git log --show-signature'
+    
+    New-Alias wspf 'ws1 pf'
+    New-Alias cdpf 'cd F:\repos\portfolio'
+
+    New-Alias cdws 'cd F:\repos\workstation1'
 }
